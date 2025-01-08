7d6dc4e1edc35a4f6e5f17cfc9f4790acf7e16f3
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2025-01-08 16:12:06 -0300

    feat(backend): organize code

--- backend/main.go
+++ backend/main.go
@@ -2,60 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"backend/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		fmt.Println(string(p))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		_, err := fmt.Fprintf(w, "%+v\n", err)
+		if err != nil {
+			return
+		}
 	}
 
-	reader(ws)
+	client := websocket.NewClient(conn, pool)
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
 
-		if err != nil {
-			return
-		}
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
-	http.HandleFunc("/ws", serveWs)
+
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
---
+++ backend/pkg/websocket/client.go
@@ -0,0 +1,56 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync/atomic"
+
+	"github.com/gorilla/websocket"
+)
+
+var clientIdCounter uint64
+
+type Client struct {
+	ID   string
+	Conn *websocket.Conn
+	Pool *Pool
+}
+
+type Message struct {
+	Type int    `json:"type"`
+	Body string `json:"body"`
+}
+
+func NewClient(conn *websocket.Conn, pool *Pool) *Client {
+	id := atomic.AddUint64(&clientIdCounter, 1)
+	return &Client{
+		ID:   fmt.Sprintf("%d", id),
+		Conn: conn,
+		Pool: pool,
+	}
+}
+
+func (c *Client) String() string {
+	return fmt.Sprintf("Client ID: %s", c.ID)
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		err := c.Conn.Close()
+		if err != nil {
+			return
+		}
+	}()
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Broadcast <- message
+		log.Printf("Message Received: %+v\n", message)
+	}
+}
